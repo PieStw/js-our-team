f75579c6b02c1ae8dbac72b3ecc9053c05d655b4
PieStw <piero.stalteri@icloud.com>
2024-10-29 15:27:18 +0100

    creazione carte dinamica

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+let team = [];
+
+
+team.push(member = {nome:`Marco Bianchi`, ruolo: "Designer", email: "marcobianchi@team.com", img: "./img/male1.png"});
+team.push(member = {nome:`Laura Rossi: `, ruolo: "Front-end Developer", email: "laurarossi@team.com", img: "./img/female1.png"});
+team.push(member = {nome:`Giorgio Verdi`, ruolo: "Back-end Developer", email: "giorgioverdi@team.com", img: "./img/male2.png"});
+team.push(member = {nome:`Marta Ipsum`, ruolo: "Seo Specialist", email: "martaipsum@team.com", img: "./img/female2.png"});
+team.push(member = {nome:`Roberto Lorem`, ruolo: "Seo Specialist", email: "robertolorem@team.com", img: "./img/male3.png"});
+team.push(member = {nome:`Daniela Amet`, ruolo: "Analyst", email: "danielaamet@team.com", img: "./img/female3.png"});
+
+
+for(let i = 0; i < team.length; i++){
+  document.getElementById("card-div").innerHTML += `<div class="card d-flex flex-row">
+                                                      <img src="${team[i].img}" class="img">
+                                                      <div class="card-body">
+                                                        <p class="card-text">${team[i].nome.toUpperCase()}</p>
+                                                        <p class="card-text">${team[i].ruolo}</p>
+                                                        <p class="card-text">${team[i].email}</p>
+                                                      </div>
+                                                    </div>`;
+}
